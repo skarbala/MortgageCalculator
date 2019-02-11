84c2cc798e82ba1bb8f1ea4691e55e3f89087c33
martin.skarbala <skarbala.martin@gmail.com>
2019-02-11 12:35:09 +0100

    feat: add modal updated style added new funds

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+Vue.component('modal', {
+    template: '#modal-template'
+});
 const app = new Vue({
     el: '#app',
     created: function () {
@@ -8,7 +11,7 @@ const app = new Vue({
     data: {
         products: [],
         message: 'Savings calculator',
-        selectedConfiguration: {
+        newSaving: {
             fund: '',
             oneTimeInvestment: '',
             totalIncome: '',
@@ -19,7 +22,9 @@ const app = new Vue({
             email: ''
         },
         calculated: false,
-        appliedSavings:[]
+        appliedSavings: [],
+        selectedSaving: '',
+        showModal: false,
         // appliedSavings: [{
         //     email: "skarbala.martin@gmail.com",
         //     oneTimeInvestment: 5000,
@@ -39,10 +44,10 @@ const app = new Vue({
     computed: {
         savingButtonDisabled: function () {
             return !(
-                this.selectedConfiguration.fund &&
-                this.selectedConfiguration.oneTimeInvestment &&
-                this.selectedConfiguration.years &&
-                this.selectedConfiguration.email
+                this.newSaving.fund &&
+                this.newSaving.oneTimeInvestment &&
+                this.newSaving.years &&
+                this.newSaving.email
             );
         }
     },
@@ -50,44 +55,45 @@ const app = new Vue({
 
     methods: {
         calculate: function () {
-            if (validate(this.selectedConfiguration)) {
-                this.selectedConfiguration.totalIncome = calculateFinalSaving(
-                    this.selectedConfiguration.oneTimeInvestment,
-                    this.selectedConfiguration.fund.interestRate,
-                    this.selectedConfiguration.years
+            if (validate(this.newSaving)) {
+                this.newSaving.totalIncome = calculateFinalSaving(
+                    this.newSaving.oneTimeInvestment,
+                    this.newSaving.fund.interestRate,
+                    this.newSaving.years
                 );
 
-                this.selectedConfiguration.interestIncome =
-                    this.selectedConfiguration.totalIncome -
-                    this.selectedConfiguration.oneTimeInvestment;
+                this.newSaving.interestIncome =
+                    this.newSaving.totalIncome -
+                    this.newSaving.oneTimeInvestment;
 
-                this.selectedConfiguration.taxes = calculateTaxes(this.selectedConfiguration.interestIncome);
-                this.selectedConfiguration.netIncome = this.selectedConfiguration.interestIncome -
-                    this.selectedConfiguration.taxes;
+                this.newSaving.taxes = calculateTaxes(this.newSaving.interestIncome);
+                this.newSaving.netIncome = this.newSaving.totalIncome -
+                    this.newSaving.taxes;
                 this.calculated = true;
             }
         },
         applyForSaving: function () {
-            if (validate(this.selectedConfiguration)) {
+            if (validate(this.newSaving)) {
                 const configurationToAdd = {
-                    selectedFund: this.selectedConfiguration.fund,
-                    years: this.selectedConfiguration.years,
-                    oneTimeInvestment: this.selectedConfiguration.oneTimeInvestment,
-                    totalIncome: this.selectedConfiguration.totalIncome,
-                    email: this.selectedConfiguration.email,
-                    netIncome: this.selectedConfiguration.netIncome,
-                    taxes: this.selectedConfiguration.taxes,
+                    selectedFund: this.newSaving.fund,
+                    years: this.newSaving.years,
+                    oneTimeInvestment: this.newSaving.oneTimeInvestment,
+                    totalIncome: this.newSaving.totalIncome,
+                    email: this.newSaving.email,
+                    netIncome: this.newSaving.netIncome,
+                    taxes: this.newSaving.taxes,
+                    interestIncome: this.newSaving.interestIncome,
                 };
                 this.appliedSavings.unshift(configurationToAdd);
 
-                this.selectedConfiguration.fund = '';
-                this.selectedConfiguration.oneTimeInvestment = '';
-                this.selectedConfiguration.years = '';
-                this.selectedConfiguration.totalIncome = '';
-                this.selectedConfiguration.netIncome = '';
-                this.selectedConfiguration.interestIncome = '';
-                this.selectedConfiguration.taxes = '';
-                this.selectedConfiguration.email = '';
+                this.newSaving.fund = '';
+                this.newSaving.oneTimeInvestment = '';
+                this.newSaving.years = '';
+                this.newSaving.totalIncome = '';
+                this.newSaving.netIncome = '';
+                this.newSaving.interestIncome = '';
+                this.newSaving.taxes = '';
+                this.newSaving.email = '';
                 this.calculated = false;
             }
 
@@ -95,16 +101,20 @@ const app = new Vue({
         round: function (input) {
             return Math.round(input * 100) / 100
         },
-        format: function (number,) {
-            return number.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1 ');
+        format: function (number) {
+            return parseFloat(number).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1 ');
 
+        },
+        openDetail: function (saving) {
+            this.selectedSaving = saving;
+            this.showModal = true;
         }
     }
 });
 
 
-function validate(selectedConfiguration) {
-    return (selectedConfiguration.fund && selectedConfiguration.oneTimeInvestment && selectedConfiguration.years);
+function validate(newSaving) {
+    return (newSaving.fund && newSaving.oneTimeInvestment && newSaving.years);
 }
 
 function calculateFinalSaving(initialInvestment, interestRate, years) {
